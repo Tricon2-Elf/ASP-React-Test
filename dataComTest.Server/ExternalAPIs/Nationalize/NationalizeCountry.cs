5f4fd0ed1c12ef41505aec2691e484af269e78c9
tricon2 <158119828+tricon2@users.noreply.github.com>
2025-06-29 17:38:58 +1200

    Added CountryName to NationalizeCountry class. This is captured via the country_id provided by the API

--- dataComTest.Server/ExternalAPIs/Nationalize/NationalizeCountry.cs
+++ dataComTest.Server/ExternalAPIs/Nationalize/NationalizeCountry.cs
@@ -1,13 +1,33 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace dataComTest.Server.ExternalAPIs.Nationalize
 {
     public class NationalizeCountry
     {
         [JsonPropertyName("country_id")]
-        public string? CountryCode { get; set; }
+        public string? CountryID { get; set; }
 
         [JsonPropertyName("probability")]
         public float Probability { get; set; }
+
+        [JsonPropertyName("country_name")]
+        public string CountryName
+        {
+            get
+            {
+                try
+                {
+                    if (!string.IsNullOrWhiteSpace(CountryID))
+                        return new RegionInfo(CountryID).EnglishName;
+                    else
+                        throw new ArgumentException("Invalid Country Code");
+                }
+                catch
+                {
+                    return "Unknown";
+                }
+            }
+        }
     }
 }
