d60852a7b0b2b531f58c470ab6676bf748ffd44b
tricon2 <158119828+tricon2@users.noreply.github.com>
2025-06-29 18:31:17 +1200

    Added a AllifyResponse class for when sending the merged data to the frontend

---
+++ dataComTest.Server/AllifyResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace dataComTest.Server
+{
+    public class AllifyResponse
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("gender")]
+        public string? Gender { get; set; }
+
+        [JsonPropertyName("age")]
+        public int Age { get; set; }
+
+        [JsonPropertyName("country_id")]
+        public string? CountryID { get; set; }
+
+        [JsonPropertyName("country_name")]
+        public string CountryName => Utils.GetCountryNameByID(CountryID);
+    }
+}
