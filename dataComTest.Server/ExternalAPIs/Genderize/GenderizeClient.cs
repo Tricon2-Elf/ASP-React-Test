9d2c202fb876c699b2d8fcd067bb8087ddd6dd38
tricon2 <158119828+tricon2@users.noreply.github.com>
2025-06-29 17:50:48 +1200

    Implemented Genderize API

--- dataComTest.Server/ExternalAPIs/Genderize/GenderizeClient.cs
+++ dataComTest.Server/ExternalAPIs/Genderize/GenderizeClient.cs
@@ -1,6 +1,44 @@
-﻿namespace dataComTest.Server.ExternalAPIs.Genderize
+﻿using System.Text.Json;
+using dataComTest.Server.ExternalAPIs.Nationalize;
+
+namespace dataComTest.Server.ExternalAPIs.Genderize
 {
     public class GenderizeClient
     {
+        private static readonly string endPoint = "https://api.genderize.io/";
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public static async Task<GenderizeResponse?> GetResponseAsync(string name, string country)
+        {
+            //Check if provided name is empty/Null
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Provided name is blank");
+
+            string url = $"{endPoint}/?name={Uri.EscapeDataString(name)}";
+
+            if(!string.IsNullOrWhiteSpace(country))
+            {
+                url += $"&country_id={Uri.EscapeDataString(country)}";
+            }
+            try
+            {
+                using HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Exception: {response.StatusCode}: {response.ReasonPhrase}");
+                }
+
+                string json = await response.Content.ReadAsStringAsync();
+                GenderizeResponse result = JsonSerializer.Deserialize<GenderizeResponse>(json) ?? throw new InvalidOperationException("Deserialize returned null");
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return null;
+            }
+        }
     }
 }
--- dataComTest.Server/ExternalAPIs/Genderize/GenderizeResponse.cs
+++ dataComTest.Server/ExternalAPIs/Genderize/GenderizeResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace dataComTest.Server.ExternalAPIs.Genderize
 {
@@ -6,6 +7,7 @@ namespace dataComTest.Server.ExternalAPIs.Genderize
     {
         [JsonPropertyName("name")]
         public string? Name { get; set; }
+
 
         [JsonPropertyName("gender")]
         public string? Gender { get; set; }
@@ -15,5 +17,27 @@ namespace dataComTest.Server.ExternalAPIs.Genderize
 
         [JsonPropertyName("count")]
         public int Count { get; set; }
+
+        [JsonPropertyName("country_id")]
+        public string? CountryID { get; set; }
+
+        [JsonPropertyName("country_name")]
+        public string CountryName
+        {
+            get
+            {
+                try
+                {
+                    if (!string.IsNullOrWhiteSpace(CountryID))
+                        return new RegionInfo(CountryID).EnglishName;
+                    else
+                        throw new ArgumentException("Invalid Country Code");
+                }
+                catch
+                {
+                    return "Unknown";
+                }
+            }
+        }
     }
 }
