a810c364db9bf658aadec679289fd04c126c0c01
tricon2 <158119828+tricon2@users.noreply.github.com>
2025-06-29 18:31:53 +1200

    Added a function for GetCountryNameByID

---
+++ dataComTest.Server/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace dataComTest.Server
+{
+    public class Utils
+    {
+        public static string GetCountryNameByID(string id)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(id))
+                    return new RegionInfo(id).EnglishName;
+                else
+                    throw new ArgumentException("Invalid Country Code");
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
+    }
+}
