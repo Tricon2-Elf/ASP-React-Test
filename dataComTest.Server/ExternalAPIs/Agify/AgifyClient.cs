d09fbd1dea8b6c59868dcbbb136552d8a2895c33
tricon2 <158119828+tricon2@users.noreply.github.com>
2025-06-29 17:50:30 +1200

    Implemented Agify API.

--- dataComTest.Server/ExternalAPIs/Agify/AgifyClient.cs
+++ dataComTest.Server/ExternalAPIs/Agify/AgifyClient.cs
@@ -1,6 +1,44 @@
-﻿namespace dataComTest.Server.ExternalAPIs.Agify
+﻿using System.Text.Json;
+using dataComTest.Server.ExternalAPIs.Genderize;
+
+namespace dataComTest.Server.ExternalAPIs.Agify
 {
     public class AgifyClient
     {
+        private static readonly string endPoint = "https://api.agify.io/";
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public static async Task<AgifyResponse?> GetResponseAsync(string name, string country)
+        {
+            //Check if provided name is empty/Null
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Provided name is blank");
+
+            string url = $"{endPoint}/?name={Uri.EscapeDataString(name)}";
+
+            if (!string.IsNullOrWhiteSpace(country))
+            {
+                url += $"&country_id={Uri.EscapeDataString(country)}";
+            }
+            try
+            {
+                using HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Exception: {response.StatusCode}: {response.ReasonPhrase}");
+                }
+
+                string json = await response.Content.ReadAsStringAsync();
+                AgifyResponse result = JsonSerializer.Deserialize<AgifyResponse>(json) ?? throw new InvalidOperationException("Deserialize returned null");
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return null;
+            }
+        }
     }
 }
--- dataComTest.Server/ExternalAPIs/Agify/AgifyResponse.cs
+++ dataComTest.Server/ExternalAPIs/Agify/AgifyResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace dataComTest.Server.ExternalAPIs.Agify
 {
@@ -12,5 +13,27 @@ namespace dataComTest.Server.ExternalAPIs.Agify
 
         [JsonPropertyName("count")]
         public int Count { get; set; }
+
+        [JsonPropertyName("country_id")]
+        public string? CountryID { get; set; }
+
+        [JsonPropertyName("country_name")]
+        public string CountryName
+        {
+            get
+            {
+                try
+                {
+                    if (!string.IsNullOrWhiteSpace(CountryID))
+                        return new RegionInfo(CountryID).EnglishName;
+                    else
+                        throw new ArgumentException("Invalid Country Code");
+                }
+                catch
+                {
+                    return "Unknown";
+                }
+            }
+        }
     }
 }
